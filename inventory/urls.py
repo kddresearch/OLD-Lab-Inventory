126914a802758898c64097d48a343bdd4812a0e1
Luis Bobadilla <happystep@ksu.edu>
2019-09-08 15:25:09 -0500

    login and logout, among other changes

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+admin.autodiscover()
 
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+normalpatterns = [
+    url(r'^login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name ='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name='inventory/homepage.html'), name ='logout'),
+    url(r'^register/', views.register,name ='register'),
+    url(r'^home/', views.home, name='home'),
+]
+
+urlpatterns += normalpatterns
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login, authenticate
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
-
+# acts as app entry
 def home(request):
     return render(request, 'inventory/home.html', {})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+    # Redirect to a success page.
+
+def login(request):
+    if request.POST:
+        if 'login' in request.POST:
+            return HttpResponseRedirect('/youloggedin/')
+        else:
+            return HttpResponseRedirect('/youregistered')
+    else:
+        return render(request, 'inventory/login.html')
+
+
+def login(request):
+    if request.POST:
+        if 'login' in request.POST:
+            return HttpResponseRedirect('/youloggedin/')
+        else:
+            return HttpResponseRedirect('/youregistered')
+    else:
+        return render(request, 'inventory/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            auth_login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'inventory/register.html', {'form': form})
